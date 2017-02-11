382ac1aa05d5706cc92a952f069434c463cfe6e4
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-11 08:08:05 -0500

    new test

--- isbn_kata.rb
+++ isbn_kata.rb
@@ -2,3 +2,8 @@ def valid_isbn?(input_string)
     string_with_no_spaces_and_hyphens = remove_invalid_characters(input_string)
     correct_length?(string_with_no_spaces_and_hyphens)
 end
+
+def remove_invalid_characters(input_string)
+    no_spaces = input_string.delete(" ")
+    no_hyphens = no_spaces.delete("-")
+end
